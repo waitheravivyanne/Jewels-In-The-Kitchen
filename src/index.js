68d158b0f2b194dcdafe1d6f8d30af3588e38625
vivian waithera <vivyannekori@gmail.com>
2023-07-05 22:33:28 +0300

    change up of javascript layout

--- src/index.js
+++ src/index.js
@@ -1,115 +1,111 @@
-const url = `https://www.themealdb.com/api/json/v1/1/categories.php`
-
+const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2a725c7489msh3cfc14ebe6b7c72p194141jsn33e879b77033',
+		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+	}
+};
 
 function fetchRecipes() {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const recipeList = document.getElementById('recipes')
-            recipeList.innerHTML = '';
+  fetch(url, options) // Include options in the fetch request
+    .then((response) => response.json())
+    .then((data) => {
+      const recipeList = document.getElementById('recipes');
+      recipeList.innerHTML = '';
 
-            data.meals.forEach((meal) => {
-                const recipe = meal.strIngredient;
-                const recipeImage = meal.strMealThumb;
-                const listItem = document.createElement('li');
-                listItem.classList.add('recipe', 'item');
-                listItem.textContent = recipe;
+      data.results.forEach((recipe) => { // Access the recipes from 'results' instead of 'meals'
+        const recipeName = recipe.name;
+        const listItem = document.createElement('li');
+        listItem.classList.add('recipe', 'item');
+        listItem.textContent = recipeName;
 
-              
-
-                
-                listItem.addEventListener('click', function() {
-                    displayRecipeDetails(meal);
-                });
-                recipeList.appendChild(listItem);
-            })
-        })
-        .catch((error) => {
-            console.log('Error fetching recipes', error);
-        })
-}
-function fetchFeaturedRecipe() {
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            const featuredRecipe = document.getElementById('featured-recipe');
-            featuredRecipe.querySelector('h4').textContent = data.meals[0].strIngredient;
-            featuredRecipe.querySelector('p').textContent = data.meals[0].strDescription;
-            featuredRecipe.querySelector('img').setAttribute('src', data.meals[0].strImage);
-            featuredRecipe.querySelector('img').setAttribute('alt', 'Recipe Image');
-            
-            
-            const saveIcon = document.createElement('i');
-            saveIcon.classList.add('far', 'fa-save', 'icon');
-            saveIcon.addEventListener('click', function () {
-              saveRecipe(data.meals[0]);
-            });
-            featuredRecipe.appendChild(saveIcon);
-      
-            const likeIcon = document.createElement('i');
-            likeIcon.classList.add('far', 'fa-heart', 'icon');
-            likeIcon.addEventListener('click', function () {
-              likeRecipe(data.meals[0]);
-            });
-            featuredRecipe.appendChild(likeIcon);
-          })
-
-    
-        .catch(error => {
-            console.log('Error fetching featured recipe:', error);
+        // Add click event listener to each recipe item
+        listItem.addEventListener('click', function() {
+          displayRecipeDetails(recipe);
         });
+
+        recipeList.appendChild(listItem);
+      });
+    })
+    .catch((error) => {
+      console.log('Error fetching recipes', error);
+    });
 }
+
+function fetchFeaturedRecipe() {
+  fetch(url, options) // Include options in the fetch request
+    .then((response) => response.json())
+    .then((data) => {
+      const featuredRecipe = document.getElementById('featured-recipe');
+      featuredRecipe.querySelector('h4').textContent = data.results[0].name;
+      featuredRecipe.querySelector('p').textContent = data.results[0].description;
+      featuredRecipe.querySelector('img').setAttribute('src', data.results[0].image);
+      featuredRecipe.querySelector('img').setAttribute('alt', 'Recipe Image');
+
+      // Create clickable icons for saving and liking recipes
+      const saveIcon = document.createElement('i');
+      saveIcon.classList.add('far', 'fa-save', 'icon');
+      saveIcon.addEventListener('click', function() {
+        saveRecipe(data.results[0]);
+      });
+      featuredRecipe.appendChild(saveIcon);
+
+      const favoriteIcon = document.createElement('i');
+      favoriteIcon.classList.add('far', 'fa-heart', 'icon');
+      favoriteIcon.addEventListener('click', function() {
+        toggleFavorite(data.results[0]);
+      });
+      featuredRecipe.appendChild(favoriteIcon);
+    })
+    .catch((error) => {
+      console.log('Error fetching featured recipe:', error);
+    });
+}
+
 function displayRecipeDetails(recipe) {
-    const featuredRecipe = document.getElementById('featured-recipe');
-    featuredRecipe.querySelector('h4').textContent = recipe.strIngredient;
-    featuredRecipe.querySelector('p').textContent = recipe.strDescription;
-    featuredRecipe.querySelector('img').setAttribute('src', recipe.strImage);
-    featuredRecipe.querySelector('img').setAttribute('alt', 'Recipe Image');
+  const featuredRecipe = document.getElementById('featured-recipe');
+  featuredRecipe.querySelector('h4').textContent = recipe.name;
+  featuredRecipe.querySelector('p').textContent = recipe.description;
+  featuredRecipe.querySelector('img').setAttribute('src', recipe.image);
+  featuredRecipe.querySelector('img').setAttribute('alt', 'Recipe Image');
 }
 
 function saveRecipe(recipe) {
-    // Perform save recipe functionality
-    console.log('Recipe saved:', recipe);
-  }
-  
-  function likeRecipe(recipe) {
-    // Perform like recipe functionality
-    console.log('Recipe liked:', recipe);
-  }
-document.addEventListener('DOMContentLoaded', function () {
-    fetchRecipes();
-    fetchFeaturedRecipe();
+  // Perform save recipe functionality
+  console.log('Recipe saved:', recipe);
+}
+
+function toggleFavorite(recipe) {
+  // Toggle the favorite state of the recipe
+  console.log('Recipe toggled as favorite:', recipe);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchRecipes();
+  fetchFeaturedRecipe();
 });
+
 document.getElementById('saved-recipes').addEventListener('click', function(event) {
-    
-    const clickedElement = event.target;
-    if (clickedElement.classList.contains('recipe')) {
-        const savedRecipesList = document.getElementById('saved-recipes');
-        savedRecipesList.appendChild(clickedElement);
-    }
+  const clickedElement = event.target;
+  if (clickedElement.classList.contains('recipe')) {
+    const savedRecipesList = document.getElementById('saved-recipes');
+    savedRecipesList.appendChild(clickedElement);
+  }
 });
 
 document.getElementById('favorite-recipes').addEventListener('click', function(event) {
-    const clickedElement = event.target;
-    if (clickedElement.classList.contains('recipe')) {
-        const favoriteRecipesList = document.getElementById('favorite-recipes');
-        favoriteRecipesList.appendChild(clickedElement);
-    }
+  const clickedElement = event.target;
+  if (clickedElement.classList.contains('recipe')) {
+    const favoriteRecipesList = document.getElementById('favorite-recipes');
+    favoriteRecipesList.appendChild(clickedElement);
+  }
 });
 
-// document.getElementById('recipes').addEventListener('click', function(event) {
-//     const selectedOption = event.target.value;
-//     console.log(selectedOption);
-// });
+document.getElementById('search-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const searchInput = document.getElementById('search-input').value;
+  console.log('Search input:', searchInput);
+});
 
-document.getElementById('search-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const searchInput = document.getElementById('search-input').value;
-    console.log('Search input:', searchInput);
-})
- 
-
-
-fetchRecipes();
-
-fetchFeaturedRecipe();
+fetchRecipes(); // Remove duplicate fetchRecipes() call
