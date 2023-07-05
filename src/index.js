b2c57f4c89671d0d3ed3562deb45ca18d8e07f0b
vivian waithera <vivyannekori@gmail.com>
2023-07-05 11:19:30 +0300

    index.js manipulation

--- src/index.js
+++ src/index.js
@@ -1,41 +1,85 @@
-let url = "https://www.themealdb.com/api/json/v1/1/filter.php?a=american"
+const axios = require('axios');
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     getRecipe();
-//     recipeDetails();
-// })
+const options = {
+  method: 'GET',
+  url: 'https://yummly2.p.rapidapi.com/feeds/auto-complete',
+  params: {q: 'chicken soup'},
+  headers: {
+    'X-RapidAPI-Key': 'e504e03979msh60219c5ed647d2fp1ba80djsn858fe40fc19f',
+    'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+  }
+};
 
-// const recipeItem = document.querySelector("recipe-item")
-// const favoriteRecipes = document.getElementById('favorite-recipes')
-// const savedRecipes = document.getElementById('saved-recipes');
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
+document.addEventListener('DOMContentLoaded', function () {
+    fetchRecipes();
+    fetchFeaturedRecipe();
+});
+document.getElementById('saved-recipes').addEventListener('click', function(event){
+    const clickedElement = event.target;
+    if(clickedElement.classList.contains('recipe')){
+        const savedRecipesList = document.getElementById('saved-recipes');
+        savedRecipesList.appendChild(clickedElement);
+    }
+});
 
-// getRecipes();
-// allRecipes()
+document.getElementById('favorite-recipes').addEventListener('click',function (event){
+    const clickedElement = event.target;
+    if(clickedElement.classList.contains('recipe')){
+        const savedRecipesList = document.getElementById('saved-recipes');
+        savedRecipesList.appendChild(clickedElement);
+    }
+});
 
-// async function fetchRecipes(){ 
-//     try{
-//         const response = await fetch(url);
-//         const data = await response.json;
-        
-//         return data;
-//     }catch (error){
-//             console.log('Error fetching recipes:', error);
-//             return [];
-//         }
-//     }
+document.getElementById('recipes').addEventListener('change',function(event){
+    const selectedOption = event.target.value;
+    console.log('selectedOption');
+});
 
-//     function renderRecipeList(recipes){
-//         const recipesList = document.getElementById('recipes');
+document.getElementById('search-form').addEventListener('submit', function (event){
+    event.preventDefault();
+    const searchInput = document.getEventListener('search-input').value;
+    console.log('Search input:', searchInput);
+})
+function fetchRecipes() {
+    fetch('url')
+        .then(response => response.json())
+        .then(data => {
+            const recipeList = document.getElementById('recipes')
 
-//         recipesList.innerHTML = '';
-//         recipes.forEach((recipe)=>{
-//             const listItem = document.createElement('li');
-//             listItem.textContent = recipe.name;
+            recipeList.innerHTML = '';
+            data.forEach(recipe => {
+                const listItem = document.createElement('li');
+                listItem.classList.add('recipe', 'item');
+                listItem.textContent = recipe.title;
+                recipeList.appendChild(listItem);
+            })
+        })
+        .catch(error => {
+            console.log('Error fetching recipes', error);
+        })
+}
+function fetchFeaturedRecipe() {
+    fetch('url/featured-recipe')
+        .then(response => response.json())
+        .then(data => {
+            const featuredRecipe = document.getElementById('featured-recipe');
+            featuredRecipe.querySelector('h4').textContent = data.title;
+            featuredRecipe.querySelector('p').textContent = data.description;
+            featuredRecipe.querySelector('img').setAttribute('src', data.image);
+            featuredRecipe.querySelector('img').setAttribute('alt', 'Recipe Image');
+        })
+        .catch(error => {
+            console.log('Error fetching featured recipe:', error);
+        });
+}
 
-//             const saveIcon = document.createElement('i');
-//             saveIcon.className = 'save-icon fas fa-bookmark';
 
-//             listItem.appendChild(saveIcon);
-//             recipesList.appendChild(listItem)
-//         })
-//     }
+fetchRecipes();
+
+fetchFeaturedRecipe();
